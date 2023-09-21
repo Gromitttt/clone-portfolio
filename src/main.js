2ee9529935135b3865b129c73c584a19329e697b
Gromitttt <141978804+Gromitttt@users.noreply.github.com>
2023-09-21 15:40:13 +0900

    JS : arrow up opacity

--- src/main.js
+++ src/main.js
@@ -1,17 +1,40 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 
-document.addEventListener('scroll', () => {
+function handleDarkMode() {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark')
   }
+}
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+function handleHomeOpacity() {
+  const opacity = 1 - window.scrollY / homeHeight;
+  home.style.opacity = opacity;
+}
+
+const arrowUp = document.querySelector('.arrow-up');
+
+function handleArrowVisibility() {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('arrow-up-visible');
+  } else {
+    arrowUp.classList.remove('arrow-up-visible');
+  }
+}
+
+document.addEventListener('scroll', () => {
+  handleDarkMode();
+  handleHomeOpacity();
+  handleArrowVisibility();
 });
 
-  const home = document.querySelector('.home__container');
-  const homeHeight = home.offsetHeight;
-  document.addEventListener('scroll', () => {
-    const opacity = 1 - window.scrollY / homeHeight;
-    home.style.opacity = opacity;
-  })
+
+
+
+
+
