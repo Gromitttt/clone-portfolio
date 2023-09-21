eb7f6d0e4c193f93bbdbef8fec7e91e8fbcbc12f
Gromitttt <141978804+Gromitttt@users.noreply.github.com>
2023-09-21 22:23:12 +0900

    JS : projects

--- src/project.js
+++ src/project.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const categories = document.querySelector('.categories');
+const btnCategories = document.querySelectorAll('.category');
+const projectsContainer = document.querySelector('.projects');
+const projects = document.querySelectorAll('.project');
+
+let prevCategory = 'all';
+
+categories.addEventListener('click', (e) => {
+  const filter = e.target.dataset.category;
+  if (!filter || filter === prevCategory) return;
+
+  handleActiveSelection(e.target);  
+  filterProjects(filter);
+  animationOut();
+
+  prevCategory = filter;
+});
+
+function filterProjects(type) {
+  projects.forEach((project) => {
+    project.style.display = type === 'all' || type === project.dataset.category ? 'block' : 'none'; 
+  });
+}
+
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function animationOut() {
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+}
+
+
+
